69d76cb1fb5d98fb8f4bcbe5ff39ae69888c2198
mininet <mininet@mininet.(none)>
2016-01-23 02:53:17 -0800

    test 11

---
+++ test12/pox_fv_q80_test12.py
@@ -0,0 +1,72 @@
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+from pox.lib.util import dpidToStr
+
+log = core.getLogger()
+
+s1_dpid=0
+s2_dpid=0
+
+# Se metieron aca las colas
+
+def _handle_ConnectionUp(event):
+	global s1_dpid, s2_dpid
+	print "ConnectionUp: ", dpidToStr(event.connection.dpid)
+
+	#remember the connection dpid for switch 
+	for m in event.connection.features.ports:
+		if m.name == "s1-eth1":
+			s1_dpid = event.connection.dpid
+			print "s1_dpid = ", s1_dpid
+		elif m.name == "s2-eth1":
+			s2_dpid = event.connection.dpid
+			print "s2_dpid = ", s2_dpid
+
+
+def _handle_PacketIn(event):
+	global s1_dpid, s2_dpid
+	# print "PacketIn: ", dpidToStr(event.connection.dpid)
+
+	if event.connection.dpid==s1_dpid:
+	   # Flujo entrante por el puerto 2 (h1 --- s1)
+		msg = of.ofp_flow_mod()
+		msg.priority = 100				# A numero mas alto mayor prioridad 
+		msg.idle_timeout = 0
+		msg.hard_timeout = 0
+		msg.match.in_port = 2		
+		msg.actions.append(of.ofp_action_enqueue(port = 1, queue_id = 2))
+		event.connection.send(msg)
+				
+		# Flujo entrante por el puerto 1 (s1 --- s2)
+		msg = of.ofp_flow_mod()
+		msg.priority = 1
+		msg.idle_timeout = 0
+		msg.hard_timeout = 0
+		msg.match.in_port = 1		
+		msg.actions.append(of.ofp_action_output(port = 2))
+		event.connection.send(msg)
+
+	if event.connection.dpid==s2_dpid:			
+		
+		# Flujo entrante por el puerto 2 (s2 --- h2)
+		msg = of.ofp_flow_mod()
+		msg.priority = 100				# A numero mas alto mayor prioridad 
+		msg.idle_timeout = 0
+		msg.hard_timeout = 0
+		msg.match.in_port = 2		
+		msg.actions.append(of.ofp_action_enqueue(port = 1, queue_id = 2))
+		event.connection.send(msg)
+		
+		# Flujo entrante por el puerto 1 (s1 --- s2)
+		msg = of.ofp_flow_mod()
+		msg.priority = 1
+		msg.idle_timeout = 0
+		msg.hard_timeout = 0
+		msg.match.in_port = 1		
+		msg.actions.append(of.ofp_action_output(port = 2))
+		event.connection.send(msg)
+		
+def launch ():
+	core.openflow.addListenerByName("ConnectionUp", _handle_ConnectionUp)
+	core.openflow.addListenerByName("PacketIn", _handle_PacketIn) 
+
---
+++ test12/switch_queues.sh
@@ -0,0 +1,18 @@
+# switch 1
+echo "Configurando colas del switch 1"
+sudo ovs-vsctl set port s1-eth1 qos=@newqos \
+-- --id=@newqos create qos type=linux-htb \
+other-config:max-rate=100000000 queues=0=@q0,1=@q1,2=@q2 \
+-- --id=@q0 create queue other-config:min-rate=100000000 other-config:max-rate=100000000 \
+-- --id=@q1 create queue other-config:min-rate=20000000 other-config:max-rate=20000000 \
+-- --id=@q2 create queue other-config:min-rate=80000000 other-config:max-rate=80000000
+
+# switch 2
+echo "Configurando colas del switch 2"
+sudo ovs-vsctl set port s2-eth1 qos=@newqos \
+-- --id=@newqos create qos type=linux-htb \
+other-config:max-rate=100000000 queues=0=@q0,1=@q1,2=@q2 \
+-- --id=@q0 create queue other-config:min-rate=100000000 other-config:max-rate=100000000 \
+-- --id=@q1 create queue other-config:min-rate=20000000 other-config:max-rate=20000000 \
+-- --id=@q2 create queue other-config:min-rate=80000000 other-config:max-rate=80000000
+
